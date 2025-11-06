92b61bc572cae7058fced413984b47f4a017a7ec
Aaryan <np03cs4a220088@heraldcollege.edu.np>
2025-11-06 12:26:53 +0545

    bun server test

--- index.ts
+++ index.ts
@@ -1,8 +1,30 @@
-const z = 10
+import figlet from "figlet";
 
-const read = (text: string) => {
-    return text
-}
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    const url = new URL(req.url);
 
-const msg = read("Hello World")
-console.log(msg)
+    if (url.pathname === "/") {
+      const body = figlet.textSync("Hello Bun!");
+      return new Response(body);
+    }
+
+    if (url.pathname === "/about") {
+      return new Response("about me");
+    }
+
+    if (url.pathname === "/contact") {
+      return new Response("contact me at ");
+    }
+
+    //handle error
+    if (url.pathname === "/feed") {
+      throw new Error("Could not fetch feed");
+    }
+
+    return new Response("Not Found", { status: 404 });
+  },
+});
+
+console.log(`Listening on PORT http://localhost:${server.port}`);
